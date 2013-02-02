6cb54263d2414eed518cbf490a742fcf4b774268
unknown <Administrator@DADI-20120219NG.(none)>
2013-02-02 15:31:55 +0800

    修改LED IO

--- bsp/stm32f10x/led.c
+++ bsp/stm32f10x/led.c
@@ -28,11 +28,11 @@
 
 #define led1_rcc                    RCC_APB2Periph_GPIOE
 #define led1_gpio                   GPIOE
-#define led1_pin                    (GPIO_Pin_2)
+#define led1_pin                    (GPIO_Pin_0)
 
 #define led2_rcc                    RCC_APB2Periph_GPIOE
 #define led2_gpio                   GPIOE
-#define led2_pin                    (GPIO_Pin_3)
+#define led2_pin                    (GPIO_Pin_1)
 
 #endif // led define #ifdef STM32_SIMULATOR
 
